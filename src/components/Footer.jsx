41c50b7193c5306edfcf29da9be84948219a89b0
Ethan Skowronski <ethanski22@icloud.com>
2025-03-17 19:51:23 -0400

    g

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ import { profilePic } from "../assets";
 import { layout } from "../style";
 import { resumeLink, repoLink } from "../constants";
 import { AiFillGithub } from "react-icons/ai";
-import resume from "src/pdfs/EthanSkowronskiResume.pdf";
 
 const Footer = () => (
   <footer id="contactMe" className="bg-gray-900 sm:px-16 px-6">
@@ -37,7 +36,7 @@ const Footer = () => (
 
         <div className="grid grid-cols-2">
           {/* styles is a prop */}
-          <a href={resume} target="_blank" download="EthanSkowronskiResume.pdf">
+          <a href={resumeLink} target="_blank" download="EthanSkowronskiResume.pdf">
             <Button styles="mt-10 mr-3 inline-flex items-center justify-center" text="Resume" icon={AiFillGithub} />
           </a>
           <a href={repoLink} target="_blank">
